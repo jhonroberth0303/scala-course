e5a2ee57ea9af9ccda66d888ee73128569854576
CEIBA\jhon.fernandez <jhonfernandez0303@gmail.com>
2025-10-16 20:03:11 -0500

    functions

--- src/main/scala/funciones/Funciones.scala
+++ src/main/scala/funciones/Funciones.scala
@@ -12,6 +12,14 @@ class Funciones {
 
   val esPar: Int => Boolean = (x) => (if (x % 2 == 0) true else false)
 
+  var areaRectangulo: (Double, Double) => Double = (x, y) => x * y
+
+  var filterImpares: List[Int] => List[Int] = (lista) => lista.filter(x => x % 2 != 0)
+
+  var factorial: Int => Int = (n) => if (n == 0) 1 else n * factorial(n - 1)
+
   // Funciones de orden superior
   def aplicarFuncion(f: Int => Int, valor: Int): Int = f(valor)
+
+
 }
