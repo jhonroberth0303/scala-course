4855e0ff25c22cc67bc5eb91fc0fcb47f9acb98e
CEIBA\jhon.fernandez <jhonfernandez0303@gmail.com>
2025-10-17 19:31:23 -0500

    Funciones de orden superior

--- src/main/scala/funciones/Funciones.scala
+++ src/main/scala/funciones/Funciones.scala
@@ -1,7 +1,7 @@
 package com.ceiba.application
 package funciones
 
-class Funciones {
+case class Funciones() {
 
   // Funciones anonimas (funciones lambda)
   val inc: Int => Int = (x) => x + 1
@@ -20,6 +20,68 @@ class Funciones {
 
   // Funciones de orden superior
   def aplicarFuncion(f: Int => Int, valor: Int): Int = f(valor)
+
+  def aplicarDosVeces(f: Int => Int, valor: Int): Int = f(f(valor))
+
+  def resultado = aplicarDosVeces((x: Int) => x + 3, 10) // resultado es 16
+
+  def res = aplicarFuncion(inc, 5) // res es 6
+
+  // Funcion de orden superior que retorna una funcion
+
+  def crearMultiplicador(factor: Int): Int => Int = (x: Int) => x * factor
+  def duplicar = crearMultiplicador(2)
+  def triplicar = crearMultiplicador(3)
+
+  def resultadoMultiplicador = duplicar(5) // resultadoMultiplicador es 10
+  def resultadoTriplicar = triplicar(5) // resultadoTriplicar es 15
+
+  // Funcion de orden superior que recibe y retorna una funcion
+  def combinarFunciones(f: Int => Int, g: Double => Double): Int => Double = (x: Int) => g(f(x))
+  def sumar2 = combinarFunciones(inc, areaCirculo) // sumar2 es una funcion que suma 2
+  def resultadoCombinar = sumar2(4) // resultadoCombinar es 7
+
+  // Colecciones y funciones de orden superior
+
+  val numeros = List(1, 2, 3, 4, 5)
+  val cuadrados = numeros.map(x => x * x) // cuadrados es List(1, 4, 9, 16, 25)
+
+  val pares = cuadrados.filter(x => x % 2 == 0) // pares es List(4, 16)
+
+  val sumaTotal = numeros.reduce((x, y) => x + y) // sumaTotal es 15
+
+  val nombres = List("Ana", "Luis", "Pedro", "Marta", "Sofía", "Carlos", "Lucía", "Jorge", "Elena", "Diego")
+  val nombresConL = nombres.filter(x => x.contains("l") || x.contains("L")) // Nombres que contienen la letra 'l' o 'L'
+  val nombresOrdenados = nombres.sortBy(x => x.length) // Nombres ordenados por longitud
+  val nombresMayusculas = nombres.map(x => x.toUpperCase()) // Nombres en mayúsculas
+  val nombresOrdenadosAlfabeticamente = nombres.sorted // Nombres ordenados alfabéticamente
+  val nombresConcatenados = nombres.reduce((x, y) => x + ", " + y) // Nombres concatenados en una sola cadena
+  val nombresFiltradosReducidos = nombres.filter(x => x.length > 4).reduce((x, y) => x + " & " + y) // Nombres con más de 4 letras concatenados
+  val nombresContados = nombres.count(x => x.length <= 4) // Contar nombres con 4 o menos letras
+  val nombresAgrupadosPorLongitud = nombres.groupBy(x => x.length) // Agrupar nombres por longitud
+
+
+  // map, filter, filtermap, flatmap, reduce, fold, groupby, sortby, foreach, foldleft, foldright
+  
+  // 1. Incrementar con fold left
+  def product(xs: List[Int]): Int = xs.foldLeft(1)((acc, x) => acc * x)
+  
+  // Foldleft con reverse
+  def reverse[A](xs: List[A]): List[A] = xs.foldLeft(List.empty[A])((acc, x) => x :: acc)
+  
+  // Aplanarlista de listas con folright
+  val archivo = List(
+    List("Línea 1 del archivo 1", "Línea 2 del archivo 1"),
+    List("Línea 1 del archivo 2", "Línea 2 del archivo 2"),
+    List("Línea 1 del archivo 3", "Línea 2 del archivo 3")
+  )
+  
+  val lineasAplanadas: List[String] = archivo.foldRight(List.empty[String])((lineas, acc) => lineas ++ acc)
+  
+  //Demostracion que foldRight = reverse + foldLeft
+  def foldRightViaLeft[A, B](xs: List[A], z: B)(op: (A, B) => B): B = {
+    xs.reverse.foldLeft(z)((b, a) => op(a, b))
+  }
 
 
 }
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,8 +1,10 @@
 package com.ceiba.application
 
 import personas.Ingeniero
-import objetos._
+import objetos.*
 import vehiculos.Auto
+
+import com.ceiba.application.funciones.Funciones
 
 
 @main
@@ -39,3 +41,79 @@ def main(): Unit =
     case Masculino => println("Coincidió: Masculino")
     case Femenino  => println("Coincidió: Femenino")
     case SexoInvalido => println("Coincidió: Sexo inválido")
+
+  val funciones = Funciones()
+
+  println(funciones.resultadoCombinar)
+
+  val listaNumeros = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+  val responseProduct = funciones.product(listaNumeros)
+  println("FoldLeft" + responseProduct)
+
+  val responseReverse = funciones.reverse(listaNumeros)
+  println("FoldLeft reverse: " + responseReverse)
+
+  val flatenList = funciones.lineasAplanadas
+  println("Archivos aplanados: " + flatenList)
+
+  // funciones de orden superior
+
+  val nums = List(1, 2, 3, 4, 5)
+  val cuadrados = nums.map(x => x * x)
+  println("Cuadrados: " + cuadrados)
+
+  val pares = nums.filter(x => x % 2 == 0)
+  println("Pares: " + pares)
+
+  val dobles = nums.map(x => x * 2)
+  println("Dobles: " + dobles)
+
+  val suma = nums.reduce((x, y) => x + y)
+  println("Suma: " + suma)
+
+  nums.foreach(x => println(s"Número: $x"))
+
+  val count = nums.count(x => x > 3)
+  println("Count > 3: " + count)
+
+  //------ Tipos algebraicos y pattern matching
+
+  sealed trait Resultado
+
+  case class Exito(valor: Int) extends Resultado
+  case class Error(mensaje: String) extends Resultado
+
+  def manejarResultado(r: Resultado): String = r match {
+    case Exito(valor) => s"Operación exitosa con valor: $valor"
+    case Error(mensaje) => s"Operación fallida con mensaje: $mensaje"
+  }
+
+  val r1: Resultado = Exito(42)
+  val r2: Resultado = Error("Algo salió mal")
+
+  println(manejarResultado(r1)) // Operación exitosa con valor: 42
+  println(manejarResultado(r2)) // Operación fallida con mensaje: Algo salió mal
+
+
+  // recursion estructural
+
+  sealed trait Lista[+A]
+  case object Vacía extends Lista[Nothing]
+  case class Nodo[A](head: A, tail: Lista[A]) extends Lista[A]
+
+  def longLista[A](l: Lista[A]): Int = l match {
+    case Vacía => 0
+    case Nodo(_, t) => 1 + longLista(t)
+  }
+
+  // Explicación
+
+  //- `A` Es un parámetro de tipo genérico (como `T` en otros lenguajes). Representa el tipo de los elementos que guarda la `Lista`. Ejemplo: `Lista[Int]` es una lista de `Int`.
+
+  //- `+A` (covarianza) `Lista[+A]` indica que la lista es covariante en `A`. Si `Perro` es subtipo de `Animal`, entonces `Lista[Perro]` será subtipo de `Lista[Animal]`. Esto permite usar una `Lista[Perro]` donde se espera una `Lista[Animal]`. Restricción: no se pueden definir métodos que reciban `A` como parámetro (porque rompería la seguridad de tipos).
+
+  // `Nothing` `Nothing` es el tipo inferior (bottom type) en Scala: es subtipo de todos los tipos. Se usa para representar valores que nunca existen en tiempo de ejecución (p. ej. `throw`), o para declarar el vacío de una estructura genérica. `case object Vacía extends Lista[Nothing]` permite que `Vacía` sea considerada `Lista[Int]`, `Lista[String]`, etc.
+
+  // - Recursividad estructural (por qué y para qué) La definición de `Lista` es inductiva: hay un caso base (`Vacía`) y un constructor recursivo (`Nodo(head, tail)`). La función `longLista` recorre la estructura siguiendo su forma: si es `Vacía` devuelve 0; si es `Nodo`, cuenta 1 y llama recursivamente sobre `tail`. Esto se llama recursión estructural porque la función se aplica a la parte "más pequeña" (la cola) de la estructura hasta llegar al caso base. Es una forma natural de procesar estructuras inductivas (listas, árboles).
+
+  // - Nota sobre rendimiento y seguridad de pila   La implementación recursiva simple puede causar desbordamiento de pila para listas muy largas. Se puede convertir a una versión tail-recursive usando un acumulador para evitar ese problema.
